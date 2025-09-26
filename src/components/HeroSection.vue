dc42891ae31412cf9d87ea25044830a6cb4148a7
pips1804 <jhonpaulmanlapaz1804@gmail.com>
2025-09-26 20:04:22 +0800

    try commit

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -79,7 +79,7 @@
           >
             <!-- Download for Android -->
             <a
-              href="https://drive.google.com/file/d/1PZCrHyrgB3op89qu1js39El217GipzAy/view?usp=sharing"
+              href="https://drive.google.com/file/d/1GLJJ1HCeRS4o5pXl4NBP8JrFJa0KEJqR/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               class="group bg-[#116530] hover:bg-green-600 text-white px-6 sm:px-8 py-3 sm:py-4 rounded-2xl font-semibold text-base sm:text-lg flex items-center justify-center space-x-2 sm:space-x-3 transition-all duration-600 ease-out transform hover:scale-105 hover:-translate-y-1 shadow-2xl hover:shadow-3xl"
